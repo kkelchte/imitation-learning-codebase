aa0ac26428deec600c5bec4761c9a22fd6c5dadb
kkelchte <kkelchtermans@gmail.com>
2020-01-07 10:47:46 +0100

    clean up

--- test/singularity_sanity_check.py
+++ test/singularity_sanity_check.py
@@ -1,9 +1,16 @@
 import unittest
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class SingularitySanityCheck(unittest.TestCase):
+
+    def test_cuda_pytorch(self):
+        import torch
+        self.assertTrue(torch.cuda.is_available())
+
+    def test_gzserver(self):
+        import coroutines
+        coroutine
+
 
 
 if __name__ == '__main__':
