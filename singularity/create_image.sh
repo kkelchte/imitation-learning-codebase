ec6c629d1bff14b9fe97c5260ebc03970b04173b
kelchtermans <kkelchtermans@gmail.com>
2020-01-13 12:04:20 +0100

    add ros

--- singularity/create_image.sh
+++ singularity/create_image.sh
@@ -14,7 +14,7 @@ else
 fi
 
 if [ -e image-${VERSION}.sif ] ; then
-  singularity sign image-${VERSION}.sif
+  #singularity sign image-${VERSION}.sif
   echo "singularity push image-${VERSION}.sif library://kkelchte/default/ros-gazebo-cuda:${VERSION} ..."
   # TODO: -U is required as container verification takes a long time and results in a connection error
   singularity push -U image-${VERSION}.sif library://kkelchte/default/ros-gazebo-cuda:${VERSION}
