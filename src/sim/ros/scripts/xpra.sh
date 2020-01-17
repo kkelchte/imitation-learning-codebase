bedf2f495d13b4c03edc708df9bf0afe71cf9b03
kkelchte <kkelchtermans@gmail.com>
2020-01-17 10:24:24 +0100

    allow output

--- src/sim/ros/scripts/xpra.sh
+++ src/sim/ros/scripts/xpra.sh
@@ -10,4 +10,5 @@ export LD_LIBRARY_PATH=''
 xpra --xvfb="Xorg -noreset -nolisten tcp
     -config /etc/xpra/xorg.conf
     -logfile ${HOME}/.xpra/Xorg-${DISPLAY}.log" \
-    start $DISPLAY > /dev/null 2>&1 &
+    start $DISPLAY
+#    > /dev/null 2>&1 &
