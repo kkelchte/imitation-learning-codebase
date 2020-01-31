915c10aabeed701002543d3a94beacf37fa41a8b
kelchtermans <kkelchtermans@gmail.com>
2020-01-31 14:55:33 +0100

    new image 0.0.15

--- rosenvironment/singularity/build_writable_image.sh
+++ rosenvironment/singularity/build_writable_image.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 echo "
  # $ sudo su
-  # # lsblk
-  # # fdisk /dev/sda
+  # check if partition is there under /dev/sda2
+  lsblk
+  # if partition is not there, create it
+  fdisk /dev/sda
   # # : p
   # # : n
   # # : p
   # # : 2
   # # : w
-  # # lsblk
-  # # mount -t ntfs /dev/sda2  /usr/data
+  # check if partition is there under /dev/sda2
+  lsblk
+  # initialize partition with ntfs
+  mkntfs /dev/sda2
+  mkdir -p /usr/data
+  mount -t ntfs /dev/sda2  /usr/data
 "
-sudo lsblk
+#sudo lsblk
 #sudo fdisk /dev/sda
-sudo lsblk
-sudo mkntfs /dev/sda2
-sudo mkdir -p /usr/data
-sudo mount -t ntfs /dev/sda2 /usr/data
+#sudo lsblk
+#sudo mkntfs /dev/sda2
+#sudo mkdir -p /usr/data
+#sudo mount -t ntfs /dev/sda2 /usr/data
 sudo singularity build --sandbox /usr/data/writable_image /vagrant/*.sif
