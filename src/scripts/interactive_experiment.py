25e3cf79ef7bbedf40cca899bd665e517f06c57e
kelchtermans <kkelchtermans@gmail.com>
2020-04-27 14:49:23 +0200

    add dependency graph, refactor environment_runner.py into interactive_experiment.py, remove create_hdf5_train_validation.py

--- src/scripts/interactive_experiment.py
+++ src/scripts/interactive_experiment.py
@@ -1,43 +1,73 @@
 
-from dataclasses import dataclass
-from dataclasses_json import dataclass_json
-
-from src.core.config_loader import Config, Parser
-from src.core.logger import get_logger, cprint, MessageType
-from src.sim.common.environment_runner import EnvironmentRunnerConfig, EnvironmentRunner
-from src.data.dataset_saver import DataSaverConfig, DataSaver
-
 """Script for collecting dataset / evaluating a model in simulated or real environment.
 
 Script starts environment runner with dataset_saver object to store the episodes in a dataset.
 """
 
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+
+from src.core.utils import get_filename_without_extension
+from src.sim.common.actors import DnnActor
+from src.sim.common.environment import EnvironmentConfig
+from src.core.data_types import TerminationType, EnvironmentType
+from src.sim.environment_factory import EnvironmentFactory
+from src.core.config_loader import Config, Parser
+from src.core.logger import get_logger, cprint, MessageType
+from src.data.dataset_saver import DataSaverConfig, DataSaver
+
 
 @dataclass_json
 @dataclass
 class InteractiveExperimentConfig(Config):
-    runner_config: EnvironmentRunnerConfig = None
     data_saver_config: DataSaverConfig = None
+    environment_config: EnvironmentConfig = None
+    number_of_episodes: int = None
 
 
 class InteractiveExperiment:
+    """Coordinates simulated environment lifetime.
 
+    Spawns environment, loops over episodes, loops over steps in episodes.
+    """
     def __init__(self, config: InteractiveExperimentConfig):
-        self._logger = get_logger(name=__name__,
+        self._config = config
+        self._logger = get_logger(name=get_filename_without_extension(__file__),
                                   output_path=config.output_path,
                                   quite=False)
-        cprint(f'Creating data_saver.', self._logger)
+
         self._data_saver = DataSaver(config=config.data_saver_config)
-        cprint(f'Creating environment_runner.', self._logger)
-        self._environment_runner = EnvironmentRunner(config=config.runner_config,
-                                                     data_saver=self._data_saver)
+        self._environment = EnvironmentFactory().create(config.environment_config)
+        # actor is not used for ros-gazebo environments.
+        self._actor = None if config.environment_config.factory_key == EnvironmentType.Ros \
+            else DnnActor(config=self._config.environment_config.actor_config)
+        cprint('initiated', self._logger)
+
+    def _run_episode(self):
+        experience = self._environment.reset()
+        while experience.terminal == TerminationType.Unknown:
+            experience = self._environment.step()
+        cprint(f'environment is running', self._logger)
+        while experience.terminal == TerminationType.NotDone:
+            action = self._actor.get_action(experience.sensor_data) if self._actor is not None else None
+            experience = self._environment.step(action)  # action is not used for ros-gazebo environments.
+            if self._data_saver is not None:
+                self._data_saver.save(experience=experience)
+        cprint(f'environment is terminated with {experience.terminal.name}', self._logger)
+        if self._data_saver is not None:
+            self._data_saver.save(experience=experience)
 
     def run(self):
-        cprint(f'Run environment_runner.', self._logger)
-        self._environment_runner.run()
+        for self._run_index in range(self._config.number_of_episodes):
+            cprint(f'start episode {self._run_index}', self._logger)
+            if self._run_index > 0:
+                self._data_saver.update_saving_directory()
+            self._run_episode()
+        if self._data_saver is not None:
+            self._data_saver.create_train_validation_hdf5_files()
 
     def shutdown(self):
-        result = self._environment_runner.shutdown()
+        result = self._environment.remove()
         cprint(f'Terminated successfully? {result}', self._logger,
                msg_type=MessageType.info if result else MessageType.warning)
 
