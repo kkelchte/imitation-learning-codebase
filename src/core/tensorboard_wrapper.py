be4266e3adc13143d06dc58e32eaf5200cd21fe1
kelchtermans <kkelchtermans@gmail.com>
2020-05-26 11:14:26 +0200

    add separate results file

--- src/core/tensorboard_wrapper.py
+++ src/core/tensorboard_wrapper.py
@@ -1,3 +1,4 @@
+import os
 
 from torch.utils.tensorboard import SummaryWriter
 
@@ -9,6 +10,7 @@ class TensorboardWrapper(SummaryWriter):
     def __init__(self, log_dir: str):
         super().__init__(log_dir)
         self.step = 0
+        self._output_file = os.path.join(log_dir, 'results.txt')
 
     def set_step(self, step: int):
         self.step = step
@@ -16,11 +18,16 @@ class TensorboardWrapper(SummaryWriter):
     def increment_step(self):
         self.step += 1
 
+    def _add_scalar(self, tag: str, value: float) -> None:
+        with open(self._output_file, 'a') as f:
+            f.write(f"{self.step} : {tag} = {value} \n")
+        self.add_scalar(tag, value, global_step=self.step)
+
     def write_distribution(self, distribution: Distribution, tag: str = ""):
-        self.add_scalar(f"{tag}_mean" if tag != "" else "mean", distribution.mean, global_step=self.step)
-        self.add_scalar(f"{tag}_std" if tag != "" else "std", distribution.std, global_step=self.step)
-        self.add_scalar(f"{tag}_min" if tag != "" else "min", distribution.min, global_step=self.step)
-        self.add_scalar(f"{tag}_max" if tag != "" else "max", distribution.max, global_step=self.step)
+        self._add_scalar(f"{tag}_mean" if tag != "" else "mean", distribution.mean)
+        self._add_scalar(f"{tag}_std" if tag != "" else "std", distribution.std)
+        self._add_scalar(f"{tag}_min" if tag != "" else "min", distribution.min)
+        self._add_scalar(f"{tag}_max" if tag != "" else "max", distribution.max)
 
     def write_scalar(self, data: float, tag: str):
-        self.add_scalar(tag, data, global_step=self.step)
+        self._add_scalar(tag, data)
