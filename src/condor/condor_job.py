c16fabdc383133d23551ff598098f51eec4bbe93
kelchtermans <kkelchtermans@gmail.com>
2020-05-27 15:10:46 +0200

    add green list option for ROS evaluation

--- src/condor/condor_job.py
+++ src/condor/condor_job.py
@@ -34,6 +34,7 @@ class CondorJobConfig(Config):
     gpu_mem_mb: int = 1900
     black_list: Optional[List] = None
     green_list: Optional[List] = None
+    use_green_list: bool = False
     use_singularity: bool = True
     singularity_file: str = sorted(glob.glob(f'{os.environ["PWD"]}/rosenvironment/singularity/*.sif'))[-1]
     check_if_ros_already_in_use: bool = False
@@ -43,7 +44,12 @@ class CondorJobConfig(Config):
         if self.black_list is None:
             del self.black_list
         if self.green_list is None:
-            del self.green_list
+            if self.use_green_list:
+                with open(os.path.join(self.codebase_dir, 'src/condor/config/green_list.yml')) as f:
+                    data = yaml.load(f, Loader=yaml.FullLoader)
+                self.green_list = data['green_list']
+            else:
+                del self.green_list
 
     def post_init(self):  # add default options
         if not self.output_path.startswith('/'):
