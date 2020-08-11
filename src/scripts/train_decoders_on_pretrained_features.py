8ada7a2ccc92cea3bc6278e3841f331001a5b812
kelchtermans <kkelchtermans@gmail.com>
2020-08-11 10:35:52 +0200

    add end-to-end, store encoder state_dict

--- src/scripts/train_decoders_on_pretrained_features.py
+++ src/scripts/train_decoders_on_pretrained_features.py
@@ -11,10 +11,12 @@ from tqdm import tqdm
 import visualpriors
 from visualpriors.taskonomy_network import TaskonomyDecoder
 import subprocess
+from visualpriors.transforms import VisualPriorRepresentation
 
 from src.ai.utils import mlp_creator
 from src.data.utils import load_data_from_directory
 from src.core.utils import get_date_time_tag
+
 
 class Parser(argparse.ArgumentParser):
     """Parser class to get config retrieve config file argument"""
@@ -28,7 +30,7 @@ class Parser(argparse.ArgumentParser):
         self.add_argument("--task", type=str, default='normal')
         self.add_argument("--loss", type=str, default='cross-entropy', help='options: cross-entropy, L1')
         self.add_argument("--mlp", action='store_true', help="use simple MLP instead of taskonomy decoder")
-
+        self.add_argument("--end_to_end", action='store_true', help="allow encoder net to change (with lower LR)")
 
 
 if __name__ == '__main__':
@@ -57,6 +59,12 @@ if __name__ == '__main__':
     print(f'{get_date_time_tag()}: Define network')
     input_size = 8 * 16 * 16
     output_size = 64 * 64
+
+    encoder = VisualPriorRepresentation.feature_task_to_net[feature_type]
+    if arguments.end_to_end:
+        for p in encoder.parameters():
+            p.requires_grad = True
+
     if arguments.mlp:
         decoder = mlp_creator(sizes=[input_size, 2056, 2056, output_size],
                               activation=nn.ReLU,
@@ -64,9 +72,9 @@ if __name__ == '__main__':
                               bias_in_last_layer=False)
     else:
         decoder = TaskonomyDecoder(out_channels=1,
-                                    is_decoder_mlp=False,
-                                    apply_tanh=True, 
-                                    eval_only=False)
+                                   is_decoder_mlp=False,
+                                   apply_tanh=True,
+                                   eval_only=False)
         for p in decoder.parameters():
             p.requires_grad = True
 
@@ -76,30 +84,34 @@ if __name__ == '__main__':
     print(f'{get_date_time_tag()}: Take {arguments.training_epochs} training steps')
     
     total = len(h5py_file['dataset']['observations']) - 100
+    encoder_optimizer = torch.optim.Adam(params=encoder.parameters(), lr=arguments.learning_rate/10)
     optimizer = torch.optim.Adam(params=decoder.parameters(), lr=arguments.learning_rate)
     losses = []
     for epoch in tqdm(range(arguments.training_epochs)):
         optimizer.zero_grad()
+        encoder_optimizer.zero_grad()
         sample_indices = np.random.choice(list(range(total)),
                                           size=arguments.batch_size)
         observations = [torch.as_tensor(h5py_file['dataset']['observations'][i], dtype=torch.float32) for i in
                         sample_indices]
-        targets = torch.stack([torch.as_tensor(h5py_file['dataset']['targets'][0].repeat(2, axis=1).repeat(2, axis=2), dtype=torch.float32) for i in
-                            sample_indices]).detach()
+        targets = torch.stack([torch.as_tensor(h5py_file['dataset']['targets'][i].repeat(2, axis=1).repeat(2, axis=2),
+                                               dtype=torch.float32) for i in sample_indices]).detach()
         
-        representation = visualpriors.representation_transform(torch.stack(observations), feature_type, device='cpu')
+        representation = encoder(torch.stack(observations))
         if arguments.mlp:
             predictions = decoder(representation.view(-1, 2048)).view(-1, 64, 64)
         else:
             predictions = (decoder(representation) + 1)/2
 
         if arguments.loss == 'L1':
-            training_loss = (torch.stack(targets) - predictions[:, :, ::2, ::2]).abs().sum()
+            training_loss = (targets - predictions[:, :, ::2, ::2]).abs().sum()
         else:
-            training_loss =  (-targets * predictions.log() - (1 - targets) * (1 - predictions).log()).mean()
+            training_loss = (-targets * predictions.log() - (1 - targets) * (1 - predictions).log()).mean()
         
         training_loss.backward()
         optimizer.step()
+        if arguments.end_to_end:
+            encoder_optimizer.step()
         losses.append(training_loss.item())
         writer.write_scalar(training_loss.item(), 'training_loss')
         writer.increment_step()
@@ -117,7 +129,7 @@ if __name__ == '__main__':
     os.makedirs(output_dir, exist_ok=True)
 
     data = [torch.as_tensor(t, dtype=torch.float32) for t in h5py_file['dataset']['observations'][-100::2]]
-    representation = visualpriors.representation_transform(torch.stack(data), feature_type, device='cpu')
+    representation = encoder(torch.stack(data))
     with torch.no_grad():
         if arguments.mlp:
             predictions = decoder(representation.view(-1, 2048)).view(-1, 64, 64).detach().numpy()
@@ -159,7 +171,7 @@ if __name__ == '__main__':
 
     for run in validation_runs[::3]:
         data = load_data_from_directory(run, size=(3, 256, 256))[1][::10]
-        representation = visualpriors.representation_transform(torch.stack(data), feature_type, device='cpu')
+        representation = encoder(torch.stack(data))
         with torch.no_grad():
             if arguments.mlp:
                 predictions = decoder(representation.view(-1, 2048)).view(-1, 64, 64).detach().numpy()
@@ -190,6 +202,7 @@ if __name__ == '__main__':
     ################################################################################
     print(f'{get_date_time_tag()}: Save checkpoint')
     
-    torch.save({'state_dict': decoder.state_dict()},
+    torch.save({'decoder': {'state_dict': decoder.state_dict()},
+                'encoder': {'state_dict': encoder.state_dict()}},
                f'{output_path}/checkpoint.ckpt')
     print(f'{get_date_time_tag()}: done')
