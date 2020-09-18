1ae533650822be9977be8817af1105953fbc8603
kelchtermans <kkelchtermans@gmail.com>
2020-09-18 15:35:48 +0200

    unet

---
+++ src/ai/architectures/u_net.py
@@ -0,0 +1,139 @@
+#!/bin/python3.8
+"""
+U-net implementation
+Source: https://github.com/milesial/Pytorch-UNet
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from src.ai.base_net import BaseNet, ArchitectureConfig
+from src.core.data_types import Action
+from src.core.logger import get_logger, cprint
+from src.core.utils import get_filename_without_extension
+
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        # if you have padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+""" Full assembly of the parts to form the complete network """
+
+
+class Net(BaseNet):
+
+    def __init__(self, config: ArchitectureConfig, quiet: bool = False):
+        super().__init__(config=config, quiet=True)
+        self._logger = get_logger(name=get_filename_without_extension(__file__),
+                                  output_path=config.output_path,
+                                  quiet=False)
+        if not quiet:
+            cprint(f'Started.', self._logger)
+        self.input_size = (1, 64, 64)
+        self.input_scope = 'default'
+        self.output_size = (64, 64)
+        self.discrete = False
+        self.dropout = nn.Dropout(p=config.dropout) if config.dropout != 'default' else None
+
+        self.inc = DoubleConv(1, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+
+        factor = 2
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear=True)
+        self.up2 = Up(512, 256 // factor, bilinear=True)
+        self.up3 = Up(256, 128 // factor, bilinear=True)
+        self.up4 = Up(128, 64, bilinear=True)
+        self.outc = OutConv(64, 1)
+        self.initialize_architecture()
+
+    def forward(self, inputs, train: bool = False) -> torch.Tensor:
+        inputs = super().forward(inputs=inputs, train=train)
+
+        x1 = self.inc(inputs)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        if self.dropout is not None and train:
+            x5 = self.dropout(x5)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outc(x)
+        return x.squeeze(dim=1)
+
+    def get_action(self, inputs, train: bool = False) -> Action:
+        raise NotImplementedError
