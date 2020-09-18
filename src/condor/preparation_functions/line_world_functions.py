0ec38568be9a9175e584df83fbb3ee2d33b07ce4
kelchtermans <kkelchtermans@gmail.com>
2020-09-18 22:37:50 +0200

    confs

--- src/condor/preparation_functions/line_world_functions.py
+++ src/condor/preparation_functions/line_world_functions.py
@@ -12,7 +12,7 @@ def prepare_lr_line_world(base_config_file: str,
                              job_config_object: CondorJobConfig,
                              number_of_jobs: int,
                              output_path: str) -> List[CondorJob]:
-    learning_rates = [0.001, 0.0001, 0.00001, 0.000001]
+    learning_rates = [0.1, 0.01, 0.001, 0.0001]
     model_paths = [os.path.join(output_path, 'models', f'lr_{lr}')
                    for lr in learning_rates]
     adjustments = {translate_keys_to_string(['output_path']): model_paths,
