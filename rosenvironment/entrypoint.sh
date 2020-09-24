ca6bf1b46dd8b682d7589cb5aa156b90cb1493fe
klaas <kkelchtermans@gmail.com>
2020-09-24 21:52:57 +0200

    following course on navigation stack, adding tf2_geometry messages to singularity img

--- rosenvironment/entrypoint.sh
+++ rosenvironment/entrypoint.sh
@@ -32,7 +32,7 @@ export LD_LIBRARY_PATH=/opt/ros/melodic/lib:${HOME}/src/sim/ros/python2_ros_ws/d
 export CMAKE_PREFIX_PATH=/opt/ros/melodic:${HOME}/src/sim/ros/python2_ros_ws/devel:${HOME}/src/sim/ros/python3_ros_ws/devel
 
 export DSO_PATH=${HOME}/src/sim/ros/dso
-
+export TURTLEBOT3_MODEL='burger'
 cd "${HOME}" || exit 1
 
 # potentially remove .singularity.d/libs from LD_LIBRARY_PATH
---
+++ src/sim/ros/tutorials/visual_navigation.md
@@ -0,0 +1,7 @@
+Learn blocks and meaning of parameters in ROS navigation stack from arxiv.org/pdf/1706.09068.pdf
+
+Demo:
+roslaunch turtlebot3_gazebo turtlebot3_house.launch
+roslaunch turtlebot3_navigation turtlebot3_navigation.launch
+
+ 
