b90423ed6b1be81b44650d9d31dfb316b15f7d13
kkelchte <kkelchtermans@gmail.com>
2020-10-01 13:55:45 +0200

    = sign

--- src/condor/preparation_functions/line_world_functions.py
+++ src/condor/preparation_functions/line_world_functions.py
@@ -15,8 +15,7 @@ def prepare_lr_architecture_line_world(base_config_file: str,
     learning_rates = [0.1, 0.01, 0.001, 0.0001, 0.00001]
     architectures = ['auto_encoder_deep_supervision', 'auto_encoder_deep_supervision_2layered']
     batch_norm = [False, True]
-    # loss = ['WeightedBinaryCrossEntropyLoss', 'MSE_loss']
-    loss = ['WeightedBinaryCrossEntropyLoss'    ]
+    loss = ['WeightedBinaryCrossEntropyLoss', 'MSE_loss']
 
     model_paths = [os.path.join(output_path, 'models', arch, 'bn' if bn else 'default', ls, f'lr_{lr}', )
                    for arch in architectures
