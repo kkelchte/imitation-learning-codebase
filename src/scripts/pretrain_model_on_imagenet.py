ad3e337c5fd7b38a9f5750cf67e0fd541a04c72f
kelchtermans <kkelchtermans@gmail.com>
2020-10-06 11:43:53 +0200

    norm

--- src/scripts/pretrain_model_on_imagenet.py
+++ src/scripts/pretrain_model_on_imagenet.py
@@ -158,16 +158,19 @@ def main():
     }))
 
     traindir = os.path.join(args.data, 'ILSVRC2012_img_train')
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
+#    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+#                                     std=[0.229, 0.224, 0.225])
+    normalize = transforms.Normalize(mean=[0.449],
+                                     std=[0.226])
+
     train_dataset = datasets.ImageFolder(
             traindir,
             transforms.Compose([
                 transforms.RandomResizedCrop(224),
                 transforms.RandomHorizontalFlip(),
+                transforms.Grayscale(num_output_channels=1),
                 transforms.ToTensor(),
                 normalize,
-                transforms.Grayscale(num_output_channels=1)
             ]))
     train_loader = torch.utils.data.DataLoader(
             train_dataset, batch_size=args.batch_size, shuffle=True,
