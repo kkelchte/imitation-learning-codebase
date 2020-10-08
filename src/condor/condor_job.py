bc382b1fcee62854af2a7ba50c29861c0258f835
kelchtermans <kkelchtermans@gmail.com>
2020-10-08 13:59:59 +0200

    ignore recast

--- src/condor/condor_job.py
+++ src/condor/condor_job.py
@@ -239,7 +239,7 @@ class CondorJob:
         lines += f"while [ $CHECKPID -eq 0 " \
                  f"-a $(date +%s) -lt $((STARTTIME + {self._config.wall_time_s} - 5 * 60)) ] ; do " \
                  f"echo $((STARTTIME + {self._config.wall_time_s} - 5 * 60 - $(date +%s))) seconds to live \n "\
-                 f"sleep 100 \n kill -0 $PROCESSID 2&>1 >> /dev/null\n CHECKPID=$?\n done\n"
+                 f"sleep 100 \n kill -0 $PROCESSID >> /dev/null\n CHECKPID=$?\n done\n"
         lines += "if [ $CHECKPID == 0 ] ; then \n" \
                  "kill -9 $PROCESSID \n echo 'kill process before end of walltime' \n " \
                  "fake-call-to-raise-error \n" \
