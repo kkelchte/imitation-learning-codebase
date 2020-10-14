ad39728c3d17e8e6bf92610a93a785c663e93a38
kelchtermans <kkelchtermans@gmail.com>
2020-10-14 17:34:41 +0200

    update confs

--- src/scripts/prepare_config.py
+++ src/scripts/prepare_config.py
@@ -4,10 +4,11 @@ from glob import glob
 import yaml
 
 from src.core.utils import get_to_root_dir
+from src.scripts.data_cleaning import DataCleaningConfig
 from src.scripts.experiment import ExperimentConfig
 
 CONFIG_DICT = {}
-CONFIG_FILENAME = "il_evaluate_interactive_cube_world.yml"
+CONFIG_FILENAME = "rl_tracking_ppo.yml"
 CONFIG_FILE = f"src/scripts/config/{CONFIG_FILENAME}"
 
 
@@ -24,8 +25,12 @@ def read_config(file_name: str) -> dict:
     return config
 
 
-def validate_config(config_dict: dict) -> None:
-    ExperimentConfig().create(config_dict=config_dict, store=False)
+def validate_config(filename) -> None:
+    config_dict = read_config(filename)
+    if not 'data_cleaning' in filename:
+        ExperimentConfig().create(config_dict=config_dict, store=False)
+    else:
+        DataCleaningConfig().create(config_dict=config_dict, store=False)
     print("validation OK")
 
 
@@ -33,7 +38,5 @@ if __name__ == "__main__":
     get_to_root_dir()
     for f in glob(f'./src/scripts/config/*.yml'):
         print(f)
-        config = read_config(f)
-        # adjust config if needed here
-        validate_config(config)
+        validate_config(f)
         # write_config(destination, base_config)
