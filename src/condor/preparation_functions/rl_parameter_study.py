39bfba16b8b7b7719ce661097b455504dcb7dd04
kelchtermans <kkelchtermans@gmail.com>
2020-10-22 15:14:43 +0200

    confs moving target

--- src/condor/preparation_functions/rl_parameter_study.py
+++ src/condor/preparation_functions/rl_parameter_study.py
@@ -39,7 +39,7 @@ def prepare_learning_rate_study(base_config_file: str,
                                 job_config_object: CondorJobConfig,
                                 number_of_jobs: int,
                                 output_path: str) -> List[CondorJob]:
-    learning_rates = [0.001, 0.0001, 0.00001, 0.000001]
+    learning_rates = [0.01, 0.001, 0.0001, 0.00001, 0.000001]
     seeds = [123 * n + 5100 for n in range(number_of_jobs)]
     model_paths = [os.path.join(output_path, 'models', f'sd_{seed}/lr_{lr}') for lr in learning_rates for seed in seeds]
     adjustments = {translate_keys_to_string(['architecture_config',
