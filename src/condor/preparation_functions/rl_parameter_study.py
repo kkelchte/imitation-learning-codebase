d299bc4a9cab2733bed0dc2a153a1ee3a40e2cc7
CyrielVD <47987234+CyrielVD@users.noreply.github.com>
2020-11-16 16:30:03 +0100

    condor adversarial phi study

--- src/condor/preparation_functions/rl_parameter_study.py
+++ src/condor/preparation_functions/rl_parameter_study.py
@@ -126,7 +126,7 @@ def prepare_phi_study(base_config_file: str,
                       number_of_jobs: int,
                       output_path: str) -> List[CondorJob]:
     phi_keys = ["gae", "reward-to-go", "return", "value-baseline"]
-    seeds = [123 * n + 5100 for n in range(number_of_jobs)]
+    seeds = [123 * n + 5961 for n in range(number_of_jobs)]
     model_paths = [os.path.join(output_path, 'models', f'sd_{seed}/phi_{x}') for x in phi_keys for seed in seeds]
     adjustments = {translate_keys_to_string(['architecture_config',
                                              'random_seed']): seeds * len(phi_keys),
