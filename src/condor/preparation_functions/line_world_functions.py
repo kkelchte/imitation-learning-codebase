90a4dd07402b126a84656efd6c4de7cae3babc50
kkelchtel <kkelchtermans@gmail.com>
2020-11-28 11:00:06 +0100

    prepare next batch

--- src/condor/preparation_functions/line_world_functions.py
+++ src/condor/preparation_functions/line_world_functions.py
@@ -104,9 +104,9 @@ def prepare_lr_discirminator_line_world(base_config_file: str,
                                         job_config_object: CondorJobConfig,
                                         number_of_jobs: int,
                                         output_path: str) -> List[CondorJob]:
-    learning_rates = [0.001, 0.0001]
-    critic_learning_rates = [0.01, 0.001, 0.0001]
-    epsilon = [0.1, 0.5, 0.9]
+    learning_rates = [0.0001]
+    critic_learning_rates = [0.01, 0.001]
+    epsilon = [0.1, 0.5]
     bns = [False]
 
     model_paths = [os.path.join(output_path, 'models', 'auto_encoder_deeply_supervised_with_discriminator',
